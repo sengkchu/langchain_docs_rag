ab2e38f7c81fbbc9c7969bb57eef03a70e84377f
sengkchu <sengkchu>
2025-06-29 06:11:53 +0000

    clean up

--- README.md
+++ README.md
@@ -1,18 +1,7 @@
-# RAG-Chatbot
-Requirements:
-https://www.youtube.com/watch?v=p4qpIgj5Zjg
-Workspace with Unity Catalog, Model Serving and Vector Index
+# Langchain/Langgraph Documentation RAG
 
-Interactive, single user cluster running 14.3 ML or higher (Gradio piece won't work with Serverless or Shared Notebook Cluster).  Suggest LTS.
+RAG Chatbot built on Langchain/Langgraph documentations using Databricks. 
 
-Code to create your own RAG Chatbot in Databricks
+Original code from these guides: https://www.youtube.com/watch?v=p4qpIgj5Zjg and https://github.com/azbarbarian2020/RAG-Chatbot
 
-Import the RAG Chatbot.dbc file which includes the following notebooks:
-
-1 Create needed Tables
-2A incremental PDF to docs_text
-2B CSV to docs_text (alternate method for website text scraping)
-3 RAG Chatbot
-4 Chatbot GUI
-
-Instructions on what code to change for your own use are shown in the demo video.
+Langchain/Langgraph documentation data scraped with Apify
---
+++ src/config.py
@@ -0,0 +1,35 @@
+"""
+Configuration file for the RAG system.
+"""
+
+from dataclasses import dataclass
+
+@dataclass
+class GlobalConfig:
+
+    # Secrets
+    project_scope_name: str = "langgraph_docs_rag"
+    databricks_key_name: str = "databricks_rag"
+    secret: str = ""
+
+    # Data storage and chunking dataframe setup
+    catalog: str = "py_docs"
+    schema: str = "default"
+    volume_name: str = "raw_data_volume"
+    source_file_name: str = "langgraph_docs.csv"
+    text_table_name: str = "langgraph_docs_text"
+    primary_key: str = "id"
+    text_column: str = "text"
+    chunk_size: int = 1000
+    chunk_overlap: int = 200
+
+    # Vector search endpoint setup
+    vector_search_endpoint_name: str = "doc_vector_endpoint"
+    index_name: str = "langgraph_docs_idx"
+    pipeline_type: str = "TRIGGERED"
+    embedding_model_endpoint_name: str = "databricks-gte-large-en"
+
+    # LLM setup
+    chat_model: str = "databricks-llama-4-maverick"
+    max_tokens: int = 500
+    rag_model_name: str = "langgraph_docs_model"
